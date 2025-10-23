5f3df0c25997b73d069528c9fd43adbb2ec362a8
c0mplex <sadasipa@gmail.com>
2025-10-23 14:43:23 -0500

    updating logic

--- ask_primen.c
+++ ask_primen.c
@@ -3,14 +3,24 @@
 int main() 
 {
     int user = 0; 
+    int is_prime = 1;
+
     printf("What is your number? : ");
     scanf("%d", &user);
-    for (int prime_check = 1; prime_check <= user; prime_check++)
-    {
-        int division = user % prime_check;
-        printf("prime_check: %i\n", prime_check);
-        printf("division: %i\n", division);
+    if (user <= 1) {
+        is_prime = 0;
+    } else {
+        for (int i = 2; i <= user / 2; i++) {
+            if (user % i == 0) {
+                is_prime = 0;
+                break;
+            }
+        }
     }
+
+    if (is_prime)
+        printf("%d is a prime number.\n", user);
+    else
+        printf("%d is not a prime number.\n", user);
     return 0;
 }
-
