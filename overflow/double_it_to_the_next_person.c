acd9c8d50f9883539f8bb0506482ac9f098fe2f6
c0mplex <sadasipa@gmail.com>
2025-11-21 10:11:19 -0500

    adding overflow

---
+++ overflow/double_it_to_the_next_person.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+  long num = 1 ;
+  while(1)
+  {
+    printf("You have $%li Do you want to double it to the next person?:", num);
+    getchar();
+    num = num * 2;
+  }
+}
---
+++ overflow/overflow.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+	float a = 1;
+	float b = 10;
+	float result = a / b;
+
+	printf("result: %f \n", result);
+}
