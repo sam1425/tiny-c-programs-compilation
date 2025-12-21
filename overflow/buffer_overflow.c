001bea145332e27f8c97b2ca6884ee4fd7149557
c0mplex <sadasipa@gmail.com>
2025-12-21 18:47:05 -0300

    testing with overflows

---
+++ overflow/buffer_overflow.c
@@ -0,0 +1,27 @@
+/* This is an example of how to not do things,
+ * this is a buffer overflow since the buffer is 26 chars of data
+ * And you are trying to access more bytes than the specified on the buffer 
+ * so the buffer overflow leaving to unexpected behaviour,
+ * it may not crash now or later but it is still memory unsafe code
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+  char sam[26];
+  char *input = "abcdefghijklmn;opqrstuvwxyz,.:";
+  printf("Buffer length: %lu\n", sizeof(sam));
+  printf("input length: %lu\n", strlen(input));
+
+  for(unsigned long i; i < strlen(input);i++)
+  {
+    printf("%lu: ",i);
+    sam[i] = input[i];
+    printf("%c\n", sam[i]);
+  }
+
+  //printf("%s",sam);
+}
